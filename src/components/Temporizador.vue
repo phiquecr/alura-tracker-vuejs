bd51a8e51a0c98c3a0f31686bab65a430931cbd5
phiquecr <hiquicr.ofc@gmail.com>
2022-07-06 13:50:54 -0300

    up: refactoring form

---
+++ src/components/Temporizador.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+    <button class="button" @click="iniciar" :disabled="disable">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar" :disabled="!disable">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  name: "TemporizadorItem",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+      disable: false,
+    };
+  },
+  methods: {
+    iniciar() {
+      this.disable = true;
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+    finalizar() {
+      this.disable = false;
+      clearInterval(this.cronometro);
+    },
+  },
+  components: { Cronometro },
+});
+</script>
